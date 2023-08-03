abb35bf878568b2c71c6bfce97f338c66ef7aa8a
Komal Mane <komal_m@tekditechnologies.com>
2023-08-03 17:40:35 +0530

    Remove some unwanted colmns

--- data-products/src/main/scala/org/sunbird/analytics/exhaust/collection/ProgressExhaustJob.scala
+++ data-products/src/main/scala/org/sunbird/analytics/exhaust/collection/ProgressExhaustJob.scala
@@ -44,9 +44,9 @@ object ProgressExhaustJob extends BaseCollectionExhaustJob {
 
   private val filterColumns = Seq("courseid", "collectionName", "batchid", "batchName", "userid",  "state", "district", "orgname", "schooludisecode", "schoolname", "board", "block", "cluster", "usertype", "usersubtype", "enrolleddate", "completedon", "certificatestatus", "completionPercentage");
   private val columnsOrder = List("Collection Id", "Collection Name", "Batch Id", "Batch Name", "User UUID", "State", "District", "Org Name", "School Id",
-    "School Name", "Block Name", "Cluster Name", "User Type", "User Sub Type", "Catgory", "Enrolment Date", "Completion Date", "Certificate Status", "Progress", "Total Score")
+    "School Name", "Block Name", "Cluster Name", "User Type", "User Sub Type", "Category", "Enrolment Date", "Completion Date", "Certificate Status", "Progress", "Total Score")
   private val columnMapping = Map("courseid" -> "Collection Id", "collectionName" -> "Collection Name", "batchid" -> "Batch Id", "batchName" -> "Batch Name", "userid" -> "User UUID",
-    "state" -> "State", "district" -> "District", "orgname" -> "Org Name", "board" -> "Catgory", "enrolleddate" -> "Enrolment Date", "completedon" -> "Completion Date",
+    "state" -> "State", "district" -> "District", "orgname" -> "Org Name", "board" -> "Category", "enrolleddate" -> "Enrolment Date", "completedon" -> "Completion Date",
     "completionPercentage" -> "Progress", "total_sum_score" -> "Total Score", "certificatestatus" -> "Certificate Status")
 
   override def processBatch(userEnrolmentDF: DataFrame, collectionBatch: CollectionBatch)(implicit spark: SparkSession, fc: FrameworkContext, config: JobConfig): DataFrame = {
